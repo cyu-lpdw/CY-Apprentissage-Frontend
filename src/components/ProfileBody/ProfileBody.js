7239988c21a2592f7c675c83201e0fe507f0c750
k-guest <k.guest.fr@gmail.com>
2022-10-25 13:23:05 +0200

    Update ProfileBody (Functionalities Profile Soon)

--- src/components/ProfileBody/ProfileBody.js
+++ src/components/ProfileBody/ProfileBody.js
@@ -14,6 +14,9 @@ function ProfileBody () {
             <div className="profile-text">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque et nulla at pretium. Nulla posuere lorem vel magna vehicula, a auctor massa eleifend. Proin non turpis fringilla, hendrerit risus ac, ullamcorper turpis. Proin fringilla metus enim, nec congue nisl volutpat nec. Nulla a dignissim eros. Aliquam ornare quis eros vel commodo. Etiam quis cursus massa. Ut finibus erat massa, sit amet tristique nibh ultricies et. Pellentesque dapibus dui ut enim lacinia cursus. Morbi non euismod leo, non tristique dui. Ut maximus ac elit et ullamcorper. Praesent vitae auctor velit. Aenean tincidunt dolor eu eros imperdiet, sed congue augue eleifend.
             </div>
+            <div className="profile-soon">
+                Nouvelles fonctionnalités bientôt disponible
+            </div>
         </div>
     );
 }
